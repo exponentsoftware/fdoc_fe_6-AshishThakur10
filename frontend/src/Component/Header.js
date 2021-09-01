6a80609ea47a183a1307133d205f2905e275b73d
AshishThakur10 <sendashishthakur@gmail.com>
2021-09-01 23:29:34 +0530

    :construction:

--- frontend/src/Component/Header.js
+++ frontend/src/Component/Header.js
@@ -1,25 +1,63 @@
+
 import React from 'react'
 import styled from 'styled-components'
 
 export default function Header(props) {
   return (
-    <div>
+    <div className="App1">
 
       <div className="App">
 
         <Text>
+          
           <img src="/images/ak.jpg" alt="are" />
-          <P>Rockstar</P>  <br />
+          <P>Rockstar</P>
           <U>A.R.Rahman  </U>
-        </Text>
-        <br />
-        <Text1>
-          Albums
-        
+          <br />
+          
 
-        </Text1>
-      </div>
-    </div>
+        </Text>
+        <h1>Albums</h1>
+      
+         </div>
+         <Text2>
+                <div className="job-tile">
+                    
+                    <img src="/images/ak.jpg" alt="are"/><P>Rockstar</P><U>{props.title}</U>
+                    
+           </div>
+           
+                <div className="job-tile">
+                    
+                    <img src="/images/ak2.jpg" alt="are" /><P>slumdog millionaire</P>
+          <U>{props.title}</U> </div>  
+                    
+                <div className="job-tile">
+                    
+                    <img src="/images/ak3.jpg" alt="are" /><P>johdha Akhbar</P>
+          <U>{props.title}</U> </div>
+                    
+                <div className="job-tile">
+                    
+                    <img src="/images/ak4.jpg" alt="are" /><P>Dil bechara</P>
+          <U>{props.title}</U></div>
+                     
+                <div className="job-tile">
+                    
+                    <img src="/images/ak5.jpg" alt="are" /> <P>highway</P>
+          <U>{props.title} </U>
+                       </div>  
+                           
+                <div className="job-tile">
+                    
+                    <img src="/images/ak6.jpg" alt="are"  /> <P>ganjini</P>
+          <U>{props.title}</U> </div>                         
+                      
+</Text2>
+           
+        </div>
+  
+   
   )
 }
 
@@ -32,7 +70,6 @@ const Text = styled.text`
     padding:10px;
     justify-content: space-around;
     flex-wrap: wrap;
-    flex-direction: row;
     
     width:22%;
     margin:0 auto;
@@ -40,33 +77,62 @@ const Text = styled.text`
     background-color: #45484b;
     color:white;
 `;
-
-const Text1 = styled.text`
-  
-  padding-right: 201px;
-  cursor: pointer;
-  font-size:24px;
-  padding-right: 245px;
-  color:white;
-    display:flex;
-    padding:10px;
-    justify-content: space-around;
-    flex-wrap: wrap;
-    flex-direction: row;
+const Text2= styled.text`
+    display: grid;
+    grid-template-columns: repeat(6, 10fr);
+    flex-grow: 5;
     
-    width:20%;
-    margin:0 auto;
     text-align: center;
-    background-color: #45484b;
-    padding-top: 34px;
-`;
+  background-color: #242121;
+  color:white;
+    gap: 40px;
+    padding: 30px;
+    transition: transform 0.2s;
+    
+    
+
+    img {
+    display: block;
+    width: 120px;
+    height: 120px;
+    background: rgb(57, 57, 62);
+    display: flex;
+    flex-direction: column;
+    color: white;
+    transition: transform 0.2s;   
+    cursor: pointer;
+    padding: 20px;
+    padding-bottom: 25px;
+  }
+
+`
+
+
+const Text3 =styled.text`
+font-size:232px;`
+
 const U = styled.u`
 
 padding-right: 63%;
 cursor: pointer;
-font-size:24px`
+font-size:24px;`
 
 
 const P = styled.p`
 padding-right:70%;
-font-size:24px`
+font-size:24px;`
+;
+// const Is =styled.i`
+// font-size:19px;
+// background-color:rgb(57,57,62);
+// padding-right:77%;
+// margin-right: 43%;
+// padding-bottom:22%;
+
+// `
+
+// ;
+
+
+  
+
